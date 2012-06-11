1c42bd58a6b56653dbd5e046e2d5fb041fdadc82
Björn Ricks <bjoern.ricks@googlemail.com>
2012-06-11 22:11:40 +0200

    Add missing import

--- quilt/push.py
+++ quilt/push.py
@@ -19,6 +19,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
 # 02110-1301 USA
 
+import os.path
+
 from quilt.command import Command
 from quilt.db import Db, Series
 from quilt.error import NoPatchesInSeries
