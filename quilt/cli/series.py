e5051dedb72c46448ba067c0c80e305abb7e7689
Björn Ricks <bjoern.ricks@googlemail.com>
2012-06-11 21:12:14 +0200

    Implement series command

--- quilt/cli/series.py
+++ quilt/cli/series.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # vim: fileencoding=utf-8 et sw=4 ts=4 tw=80:
 
 # python-quilt - A Python implementation of the quilt patch system
@@ -19,41 +18,21 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
 # 02110-1301 USA
+import os
 
-import sys
-import os.path
+from optparse import OptionParser
 
-import quilt.cli.push
-import quilt.cli.top
-import quilt.cli.series
+from quilt.db import Series
 
-from quilt.error import QuiltError
+def parse(args):
+    usage = "%prog series"
+    parser = OptionParser(usage=usage)
+    (options, pargs) = parser.parse_args(args)
 
-def usage():
-    print "%s command [--help] ..." % (os.path.basename(sys.argv[0]))
+    patches = os.environ.get("QUILT_PATCHES")
+    if not patches:
+        patches = "patches"
 
-def main():
-    if not len(sys.argv) > 1:
-        usage()
-        sys.exit(1)
-
-    cmd = sys.argv[1]
-    args = sys.argv[2:]
-
-    try:
-        if cmd == "push":
-            quilt.cli.push.parse(args)
-        elif cmd == "top":
-            quilt.cli.top.parse(args)
-        elif cmd == "series":
-            quilt.cli.series.parse(args)
-        else:
-            print "command %s not known." % cmd
-            usage()
-            sys.exit(2)
-    except QuiltError, e:
-        print e
-        sys.exit(1)
-
-if __name__ == "__main__":
-        main()
+    series = Series(patches)
+    for patch in series.patches():
+        print patch
