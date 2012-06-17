736bd4417db619b6044cb52a8531b5f95ab21cd9
Björn Ricks <bjoern.ricks@googlemail.com>
2012-06-17 17:00:41 +0200

    Add console client interface for quilt pop

---
+++ quilt/cli/pop.py
@@ -0,0 +1,44 @@
+# vim: fileencoding=utf-8 et sw=4 ts=4 tw=80:
+
+# python-quilt - A Python implementation of the quilt patch system
+#
+# Copyright (C) 2012  Björn Ricks <bjoern.ricks@googlemail.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+# 02110-1301 USA
+
+import os
+
+from optparse import OptionParser
+
+from quilt.pop import Pop
+
+def parse(args):
+    usage = "%prog pop [-a] [patch]"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-a", "--all", help="remove all applied patches",
+                      action="store_true")
+
+    (options, pargs) = parser.parse_args(args)
+
+    pop = Pop(os.getcwd(), ".pc")
+
+    if options.all:
+        pop.unapply_all()
+    elif not args:
+        pop.unapply_top_patch()
+    else:
+        pop.unapply_patch(args[0])
+
