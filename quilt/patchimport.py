7ff9c414160a981bd92cac6fbd6cafa4e2750b64
Bjoern Ricks <bjoern.ricks@intevation.de>
2012-06-19 12:35:25 +0200

    Add missing imports

--- quilt/patchimport.py
+++ quilt/patchimport.py
@@ -19,7 +19,11 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
 # 02110-1301 USA
 
+import os.path
+
 from quilt.command import Command
+from quilt.db import Db, Series
+from quilt.utils import Directory, File
 
 class Import(Command):
     """ Command class to import patches into the patch queue """
