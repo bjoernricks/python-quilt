41416ca2e70a8de6074dd75158662cac7ddccd34
Björn Ricks <bjoern.ricks@googlemail.com>
2012-07-15 14:28:58 +0200

    Implement new command
    New creates a patch and inserts it at the end of the patch list.

---
+++ quilt/cli/new.py
@@ -0,0 +1,56 @@
+# vim: fileencoding=utf-8 et sw=4 ts=4 tw=80:
+
+# python-quilt - A Python implementation of the quilt patch system
+#
+# Copyright (C) 2012  Björn Ricks <bjoern.ricks@googlemail.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+import sys
+import os.path
+
+from optparse import OptionParser
+
+from quilt.db import Series
+from quilt.patch import Patch
+from quilt.utils import File
+
+def parse(args):
+    usage = "%prog new patchname"
+    parser = OptionParser(usage=usage)
+    (options, pargs) = parser.parse_args(args)
+
+    if len(args) != 1:
+        parser.print_usage()
+        sys.exit(1)
+
+    newpatch = args[0]
+
+    patches = os.environ.get("QUILT_PATCHES")
+    if not patches:
+        patches = "patches"
+
+    series = Series(patches)
+    if series.is_patch(newpatch):
+        print >> sys.stderr, "Patch %s already exists" % newpatch
+        sys.exit(2)
+
+    patchfile = File(os.path.join(patches, newpatch))
+    patchfile.touch()
+
+    series.add_patch(Patch(newpatch))
+    series.save()
+
