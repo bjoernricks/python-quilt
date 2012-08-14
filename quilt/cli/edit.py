8fd99cbc40f283db1e6af9c86857e1d7d91a0f5e
Bjoern Ricks <bjoern.ricks@intevation.de>
2012-08-14 16:37:51 +0200

    Add quilt.cli edit command

--- quilt/cli/edit.py
+++ quilt/cli/edit.py
@@ -19,17 +19,30 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301 USA
 
-from quilt.cli.meta import find_command, list_commands
+import os
 
-import quilt.cli.add
-import quilt.cli.applied
-import quilt.cli.edit
-import quilt.cli.new
-import quilt.cli.next
-import quilt.cli.patchimport
-import quilt.cli.pop
-import quilt.cli.previous
-import quilt.cli.push
-import quilt.cli.series
-import quilt.cli.top
-import quilt.cli.unapplied
+from quilt.add import Add
+from quilt.cli.meta import Command
+from quilt.utils import SubprocessError, Process
+
+class EditCommand(Command):
+
+    usage = "%prog edit file1 [...]"
+    name = "edit"
+    min_args = 1
+
+    def run(self, options, args):
+        cwd = os.getcwd()
+
+        add = Add(cwd, self.get_pc_dir(), self.get_patches_dir())
+        add.add_files(args, ignore=True)
+
+        editor = os.environ.get("EDITOR", "vi")
+
+        for filename in args:
+            try:
+                cmd = [editor]
+                cmd.append(filename)
+                Process(cmd).run(cwd=cwd)
+            except SubprocessError, e:
+                self.exit_error(e, value=e.returncode)
