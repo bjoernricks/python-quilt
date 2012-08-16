2fea7b0146e87b94db6fe12b6d244e6b870b7a91
Bjoern Ricks <bjoern.ricks@intevation.de>
2012-08-16 19:25:00 +0200

    Improve creating the patch in refresh
    Add a Index for each changed file and create a p1 patch.

--- quilt/refresh.py
+++ quilt/refresh.py
@@ -25,6 +25,9 @@ from quilt.error import QuiltError
 from quilt.patch import Patch, Diff
 from quilt.utils import Directory, File, TmpFile
 
+INDEX_LINE = \
+    "==================================================================="
+
 class Refresh(Command):
     """ Command class to refresh (add or remove chunks) a patch
     """
@@ -48,6 +51,7 @@ class Refresh(Command):
                 raise QuiltError("No patch applied. Nothing to refresh.")
 
         pc_dir = self.quilt_pc + patch.get_name()
+        patch_file = self.quilt_patches + File(patch.get_name())
         files = pc_dir.content()[1]
 
         with TmpFile(prefix="pquilt-") as tmpfile:
@@ -57,16 +61,47 @@ class Refresh(Command):
                     continue
                 orig_file = pc_dir + File(file_name)
                 new_file = File(file_name)
+                left_label, right_label, index = self._get_labels(orig_file,
+                                                                  new_file)
+                self._write_index(tmpfile, index)
+
                 diff = Diff(orig_file, new_file)
-                diff.run(self.cwd, fd=f)
+                diff.run(self.cwd, fd=f, left_label=left_label,
+                         right_label=right_label)
 
             if tmpfile.is_empty():
                 raise QuiltError("Nothing to refresh.")
 
-            tmpfile.copy(self.quilt_patches + File(patch.get_name()))
+            tmpfile.copy(patch_file)
 
         timestamp = pc_dir + File(".timestamp")
         timestamp.touch()
 
         refresh = self.quilt_pc + File(patch.get_name() + "~refresh")
         refresh.delete_if_exists()
+
+    def _get_labels(self, old_file, new_file):
+        dir = os.path.basename(self.cwd)
+
+        old_file_name = old_file.get_name()
+        new_file_name =  new_file.get_name()
+        old_hdr = dir + ".orig/" + old_file_name
+        new_hdr = dir + "/" + new_file_name
+
+        index = new_hdr
+
+        if not old_file.exists() or old_file.is_empty():
+            old_hdr = "/dev/null"
+
+        if not new_file.exists() or new_file.is_empty():
+            old_hdr = new_hdr
+            new_hdr = "/dev/null"
+
+        return (old_hdr, new_hdr, index)
+
+    def _write_index(self, f, index):
+        f.write("Index: ")
+        f.write(index)
+        f.write("\n")
+        f.write(INDEX_LINE)
+        f.write("\n")
