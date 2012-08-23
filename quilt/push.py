088c1b4e33d32b989b9264c1092caad0164e0ceb
Bjoern Ricks <bjoern.ricks@intevation.de>
2012-08-23 20:39:05 +0200

    Refactor Push class
    Add new signals applying_patch and applied_empty_patch. Use Directory class for
    quilt_pc and quilt_patches. Also add parameter force for apply_patch and
    apply_next_patch methods.

--- quilt/push.py
+++ quilt/push.py
@@ -16,73 +16,87 @@
 
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301 USA
-
-import os.path
 
 from quilt.command import Command
 from quilt.db import Db, Series
 from quilt.error import NoPatchesInSeries, AllPatchesApplied, QuiltError
 from quilt.patch import Patch, RollbackPatch
 from quilt.signals import Signal
-from quilt.utils import SubprocessError, File
+from quilt.utils import SubprocessError, File, Directory
 
 class Push(Command):
 
     applying = Signal()
+    applying_patch = Signal()
     applied = Signal()
     applied_patch = Signal()
+    applied_empty_patch = Signal()
 
     def __init__(self, cwd, quilt_pc, quilt_patches):
         super(Push, self).__init__(cwd)
-        self.quilt_pc = quilt_pc
-        self.quilt_patches = quilt_patches
+        self.quilt_pc = Directory(quilt_pc)
+        self.quilt_patches = Directory(quilt_patches)
         self.db = Db(quilt_pc)
         self.series = Series(quilt_patches)
 
     def _apply_patch(self, patch, force=False):
         patch_name = patch.get_name()
-        prefix = os.path.join(self.quilt_pc, patch_name)
-        patch_file = os.path.join(self.quilt_patches, patch_name)
-        refresh = File(prefix + "~refresh")
+        pc_dir = self.quilt_pc + patch_name
+        patch_file = self.quilt_patches + File(patch_name)
+        refresh = File(pc_dir.get_name() + "~refresh")
 
         if refresh.exists():
             raise QuiltError("Patch %s needs to be refreshed" % \
                                   patch_name)
 
-        self.applying(patch)
+        forced = False
+        self.applying_patch(patch)
 
-        try:
-            patch.run(self.cwd, patch_dir=self.quilt_patches, backup=True,
-                      prefix=prefix)
-            refresh.delete_if_exists()
-        except SubprocessError, e:
-            refresh.touch()
+        if patch_file.exists():
+            try:
+                patch.run(self.cwd, patch_dir=self.quilt_patches, backup=True,
+                        prefix=pc_dir.get_name())
+                refresh.delete_if_exists()
+            except SubprocessError, e:
+                refresh.touch()
 
-            if not force:
-                patch = RollbackPatch(self.cwd, prefix)
-                patch.rollback()
-                patch.delete_backup()
-                raise QuiltError("Patch %s does not apply" % patch_name)
+                if not force:
+                    patch = RollbackPatch(self.cwd, pc_dir)
+                    patch.rollback()
+                    patch.delete_backup()
+                    raise QuiltError("Patch %s does not apply" % patch_name)
+                else:
+                    forced = True
 
         self.db.add_patch(patch)
 
-        if os.path.exists(prefix):
-            File(os.path.join(prefix, ".timestamp")).touch()
+        if pc_dir.exists():
+            timestamp = pc_dir + File(".timestamp")
+            timestamp.touch()
         else:
-            os.makedirs(prefix)
+            pc_dir.create()
 
-        self.applied_patch(patch)
+        if not patch_file.exists():
+            self.applied_empty_patch(patch, False)
+        elif pc_dir.is_empty():
+            self.applied_empty_patch(patch, True)
+        elif forced:
+            raise QuiltExceptions("Applied patch %s (forced; needs " \
+                                    "refresh)" % patch.get_name())
+        else:
+            self.applied_patch(patch)
 
     def _check(self):
         if not self.series.exists() or not self.series.patches():
             raise NoPatchesInSeries(self.series)
 
-    def apply_patch(self, patch_name):
+    def apply_patch(self, patch_name, force=False):
         """ Apply all patches up to patch_name """
         self._check()
-        patches = self.series.patches_until(Patch(patch_name))[:]
+        patch = Patch(patch_name)
+        patches = self.series.patches_until(patch)[:]
 
         applied = self.db.applied_patches()
         for patch in applied:
@@ -92,14 +106,16 @@ class Push(Command):
         if not patches:
             raise AllPatchesApplied(self.series, self.db.top_patch())
 
-        for patch in patches:
-            self._apply_patch(patch)
+        self.applying(patch)
+
+        for cur_patch in patches:
+            self._apply_patch(cur_patch, force)
 
         self.db.save()
 
         self.applied(self.db.top_patch())
 
-    def apply_next_patch(self):
+    def apply_next_patch(self, force=False):
         """ Apply next patch in series file """
         self._check()
         top = self.db.top_patch()
@@ -111,7 +127,9 @@ class Push(Command):
         if not patch:
             raise AllPatchesApplied(self.series, top)
 
-        self._apply_patch(patch)
+        self.applying(patch)
+
+        self._apply_patch(patch, force)
 
         self.db.save()
 
@@ -129,8 +147,10 @@ class Push(Command):
         if not patches:
             raise AllPatchesApplied(self.series, top)
 
+        self.applying(patch)
+
         for patch in patches:
-            self._apply_patch(patch)
+            self._apply_patch(patch, force)
 
         self.db.save()
 
