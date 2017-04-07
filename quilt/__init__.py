55d2be3990e8112e84c68cd33eb9183e26900c55
Björn Ricks <bjoern.ricks@gmail.com>
2017-04-07 10:26:46 +0200

    Change license from LGPLv2.1 to MIT
    Relicense the python-quilt code from LGPLv2.1 to MIT.

--- quilt/__init__.py
+++ quilt/__init__.py
@@ -2,25 +2,11 @@
 
 # python-quilt - A Python implementation of the quilt patch system
 #
-# Copyright (C) 2012  Björn Ricks <bjoern.ricks@googlemail.com>
+# Copyright (C) 2012 - 2017 Björn Ricks <bjoern.ricks@gmail.com>
 #
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
-# 02110-1301 USA
+# See LICENSE comming with the source of python-quilt for details.
 
 """ A python implementation of quilt """
 
 __version_info__ = ("0", "3", "dev1")
 __version__ = '.'.join(__version_info__)
-
